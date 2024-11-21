e17125b565bb89fd966b045da39a749af2bb6308
Michiel de Jong <michiel@pondersource.com>
2024-11-21 17:03:27 +0100

    Fix SQL

--- src/postgres.ts
+++ src/postgres.ts
@@ -2,7 +2,7 @@ import { Client } from 'pg';
 import { IStorage } from "@inrupt/solid-client-authn-node";
 class Storage implements IStorage {
   private client: Client | undefined;
-  
+
   setClient(client: Client) {
     this.client = client;
   }
@@ -13,11 +13,13 @@ class Storage implements IStorage {
 
   async get(key: string): Promise<undefined | string> {
     console.log('retrieving', key);
-    const res = await this.client?.query(`SELECT value FROM "solid_session" WHERE "key" = $1`, [ key ]);
+    const res = await this.client?.query(`SELECT "value" FROM "solid_session" WHERE "key" = $1`, [ key ]);
     console.log(res);
-    if (Array.isArray(res)) {
+    if (Array.isArray(res?.rows)) {
       try {
-        return res[res.length - 1].value;
+        const ret = res.rows[res.rows.length - 1].value;
+        console.log('returning', ret);
+        return ret;
       } catch (e) {
         console.error(e);
       }
@@ -27,7 +29,7 @@ class Storage implements IStorage {
 
   async set(key: string, value: string): Promise<void> {
     console.log('storing', key, value);
-    await this.client?.query(`INSERT INTO "solid_session" ("key", "value") VALUES ($1, $2) ON`, [
+    await this.client?.query(`INSERT INTO "solid_session" ("key", "value") VALUES ($1, $2)`, [
       key,
       value
     ]);
